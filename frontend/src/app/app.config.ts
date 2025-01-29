2258f1d1b10f04dbe6f4ac6d411abb06356251d9
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-01-29 22:05:57 +0100

    Initialization

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideAnimationsAsync(),
+  providePrimeNG({
+    theme: {
+      preset: Lara
+    }
+  }),
+  provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
+
---
+++ frontend/src/app/language/language.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language',
+  imports: [],
+  templateUrl: './language.component.html',
+  styleUrl: './language.component.scss'
+})
+export class LanguageComponent {
+
+}
