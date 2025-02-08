f1e2285c7ed34fba2bc120e79999aa1d136afb07
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-02-08 15:18:45 +0100

    Added get db context

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
+from contextlib import contextmanager
 
 # Create the database engine
 engine = create_engine(DATABASE_URL, echo=True)
@@ -10,3 +11,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class for ORM models
 Base = declarative_base()
+
+
+@contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
