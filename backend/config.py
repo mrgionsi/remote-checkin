cc83525251de80a040be66bc7e357069e088663b
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-02-10 16:21:01 +0100

    Lint database file

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,34 @@
-
 # config.py
 
+
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from .env file
+load_dotenv()
+
+
+
+"""
+Configuration module for the remote check-in system.
+This module contains configuration classes for production and testing environments.
+"""
+
 class Config:
+    """
+    Base configuration class for production environment.
+    Defines the default configurations used in production.
+    """
     # Normal configuration for production
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATABASE_URL = "postgresql://gionsi:password@192.168.178.15:5444/remotecheckin"
+    DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 
 class TestConfig(Config):
+    """
+    Configuration class for testing environment.
+    Inherits from the base Config class and overrides the configurations for testing purposes.
+    """
     # Configuration for testing
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # In-memory SQLite for testing
     TESTING = True  # Enable testing mode
--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config import Config
-from contextlib import contextmanager
+"""
+Database module for the remote check-in system.
+This module provides configuration for SQLAlchemy, including session management,
+engine creation, and a context manager for database operations.
+"""
+
+from contextlib import contextmanager  # Standard library import
+from sqlalchemy import create_engine  # Third-party imports
+from sqlalchemy.orm import sessionmaker, declarative_base  # Third-party imports
+from config import Config  # Local application import
 
 # Create the database engine
 engine = create_engine(Config.DATABASE_URL, echo=True)
@@ -18,7 +24,9 @@ def get_db():
     """
     Context manager that yields a SQLAlchemy database session.
     
-    This generator function creates a new session using SessionLocal, yields it for use within a context, and ensures that the session is properly closed after usage. It is intended to provide a transactional scope for database operations.
+    This generator function creates a new session using SessionLocal, yields it for use within a context,
+    and ensures that the session is properly closed after usage. It is intended to provide a transactional
+    scope for database operations.
     
     Yields:
         Session: A SQLAlchemy session instance for database operations.
