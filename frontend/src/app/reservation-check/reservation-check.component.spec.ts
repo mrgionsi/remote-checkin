fa6575bd9c74854242c8df7b37f6443462bb1be5
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-02-28 17:57:29 +0100

    refactor: Add Router and ActivatedRoute mocks to test setup

--- frontend/src/app/reservation-check/reservation-check.component.spec.ts
+++ frontend/src/app/reservation-check/reservation-check.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReservationCheckComponent } from './reservation-check.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { ReservationService } from '../services/reservation.service';
 
 describe('ReservationCheckComponent', () => {
   let component: ReservationCheckComponent;
   let fixture: ComponentFixture<ReservationCheckComponent>;
 
   beforeEach(async () => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const activatedRouteSpy = {
+      paramMap: of({ get: (param: string) => param === 'code' ? 'TEST123' : null })
+    };
+
     await TestBed.configureTestingModule({
-      imports: [ReservationCheckComponent]
+      imports: [ReservationCheckComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy },
+        { provide: ReservationService, useValue: jasmine.createSpyObj('ReservationService', ['getReservationById']) }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ReservationCheckComponent);
     component = fixture.componentInstance;
