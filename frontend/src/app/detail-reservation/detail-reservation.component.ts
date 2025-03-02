4215ca7e8ed7edd5a0d08246cae0a37be9c40f4b
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-03-02 13:38:55 +0100

    feat: Created component reservation details

---
+++ frontend/src/app/detail-reservation/detail-reservation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-reservation',
+  imports: [],
+  templateUrl: './detail-reservation.component.html',
+  styleUrl: './detail-reservation.component.scss'
+})
+export class DetailReservationComponent {
+
+}
