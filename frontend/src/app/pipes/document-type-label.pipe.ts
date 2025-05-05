953b87caaefe40d7193d91165291727d8979aad0
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-05-05 16:21:58 +0200

    feat: added pipe for document type visualization

--- frontend/src/app/pipes/document-type-label.pipe.ts
+++ frontend/src/app/pipes/document-type-label.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DocumentTypeLabelPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    const map: { [key: string]: string } = {
+      identity_card: 'Identity card',
+      passport: 'Passport',
+      driver_license: 'Driver’s License'
+      // Add more mappings as needed
+    };
+
+    return map[value] || value;
   }
 
 }
