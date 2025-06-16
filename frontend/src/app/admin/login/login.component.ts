3b5bb6aae115944a2877bce0e4f110195656eaf5
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-06-16 19:58:57 +0200

    feat: Updated login component

--- frontend/src/app/admin/login/login.component.ts
+++ frontend/src/app/admin/login/login.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AdminLoginService } from '../../services/admin-login.service'; // importa il servizio
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +14,26 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   showPassword = false;
+  errorMessage: string | null = null;
+
+  constructor(
+    private adminLoginService: AdminLoginService,
+    private router: Router
+  ) { }
+
   onLogin() {
-    // Qui puoi aggiungere la logica di autenticazione
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
+    this.errorMessage = null;
+    this.adminLoginService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        console.log(res)
+        // Salva il token dove preferisci (es: localStorage)
+        localStorage.setItem('admin_token', res.access_token);
+        // Naviga alla home admin o dove vuoi
+        this.router.navigate(['/admin']);
+      },
+      error: (err) => {
+        this.errorMessage = err.error?.error || 'Errore di autenticazione';
+      }
+    });
   }
 }
