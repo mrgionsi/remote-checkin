248779d086e8e2beddcacfb3b19dd53460563938
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-07-29 19:54:46 +0200

    fix: removed console.log

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -6,11 +6,8 @@ export const authGuard: CanActivateFn = () => {
     const authService = inject(AuthService);
     const router = inject(Router);
 
-    console.log('AuthGuard is checking if user is logged in');
-    console.log(authService.isLoggedIn());
     if (!authService.isLoggedIn()) {
 
-        console.log('User is not logged in, redirecting to login page');
         //return router.createUrlTree(['/admin/login']);
         return false;
     }
