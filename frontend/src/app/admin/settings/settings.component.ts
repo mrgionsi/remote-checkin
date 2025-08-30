e428db279d91cb20d56c6eb2605857df16b9af8f
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-08-30 14:32:02 +0200

    feat: created in settings language option

--- frontend/src/app/admin/settings/settings.component.ts
+++ frontend/src/app/admin/settings/settings.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { TranslocoPipe, TranslocoService } from '@jsverse/transloco';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  standalone: true,
+  imports: [DropdownModule, FormsModule, TranslocoPipe, CommonModule, ButtonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  languages = [
+    { label: 'English', value: 'en' },
+    { label: 'Italiano', value: 'it' },
+    { label: 'Español', value: 'es' },
+    { label: 'Français', value: 'fr' },
+    { label: 'Deutsch', value: 'de' }
+  ];
+  selectedLang = localStorage.getItem('appLang') || 'en';
+  saved = false;
 
+  constructor(private translocoService: TranslocoService) { }
+
+  saveLanguage() {
+    localStorage.setItem('appLang', this.selectedLang);
+    this.translocoService.setActiveLang(this.selectedLang); // Cambia la lingua dell'app
+    this.saved = true;
+  }
 }
