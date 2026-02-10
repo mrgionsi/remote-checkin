e6f333537b3409073cbe1ee5b9e4832f3df4767b
Giovanni Pasquariello <pasquariello.giovanni@ericsson.com>
2026-02-10 11:45:44 +0100

    feat: Updated url for backend

--- frontend/src/environments/environments.preprod.ts
+++ frontend/src/environments/environments.preprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'http://localhost:4200',
   production: false,
-  apiBaseUrl: 'https://remote-checkin-be.gionsihome.duckdns.org',  // Example API URL for production
+  apiBaseUrl: 'https://remote-checkin-be.gionsi.me',  // Example API URL for production
   development: false  // Flag to enable development features
 };
